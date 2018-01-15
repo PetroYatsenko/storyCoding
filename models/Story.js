18ca52e80a222d572fda26b0fdc5f797e098711e
Petro Yatsenko <p_yatsenko@ukr.net>
2018-01-16 00:39:54 +0200

    SC inner_eater images were added & lesson 3-button view case added

--- models/Story.js
+++ models/Story.js
@@ -4,11 +4,10 @@ const storySchema = new mongoose.Schema({
   story: { type: String, match: /[a-z,A-Z]/ },
   type: {type: String, match: /[a-z,A-Z]/},
   img: {type: String},
-  next_path: {type: String},
+  topic: {type: String},
+  subject: {type: String},
   items_uk: { type: Array, match: /[a-z,A-Z]/ },
   state_uk: {type: Object},
 });
 
-const Story = mongoose.model('Story', storySchema);
-
-module.exports = Story;
+module.exports = mongoose.model('Story', storySchema);
