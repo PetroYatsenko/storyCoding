7561d350329179436ed42eca8b72301b53a29924
Petro Yatsenko <p_yatsenko@ukr.net>
2018-02-12 23:47:30 +0200

    SC remove Mongolab path and env.example

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ const app = express();
  * Connect to MongoDB.
  */
 mongoose.Promise = require('bluebird');
-mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI, {
+mongoose.connect(process.env.MONGODB_URI, {
   useMongoClient: true,
   /* other options */
 });
@@ -80,7 +80,7 @@ app.use(session({
   saveUninitialized: true,
   secret: process.env.SESSION_SECRET,
   store: new MongoStore({
-    url: process.env.MONGODB_URI || process.env.MONGOLAB_URI,
+    url: process.env.MONGODB_URI,
     autoReconnect: true,
     clear_interval: 3600
   })
