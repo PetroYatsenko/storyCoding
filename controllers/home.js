896f5dd1700b84860e4ddeff894547b0b2af0416
Petro Yatsenko <p_yatsenko@ukr.net>
2018-02-17 22:16:01 +0200

    SC heroe`s collection names update & home page monsters fix

--- controllers/home.js
+++ controllers/home.js
@@ -1,11 +1,12 @@
 const Monster = require('../models/Monster');
 
 exports.index = (req, res) => {
-  var query = {};
+  var state = 'state_' + res.locals.lang; 
   
-  Monster.find(query).then(function(monsters) {
+  Monster.find({enabled: true}, {enabled: 0, _id: 0}).then(function(monsters) {
     res.render('home', {
       title: 'Home',
+      state: state,
       monsters: monsters
     });
   });
