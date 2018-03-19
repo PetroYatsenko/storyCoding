406a08557b560acc51f126643d29e2d31790294e
Petro Yatsenko <p_yatsenko@ukr.net>
2018-03-19 19:07:21 +0200

    SC improve dashboard links generation

--- routes/lessons.js
+++ routes/lessons.js
@@ -6,9 +6,9 @@ const lessonController = require('../controllers/lesson');
 /* General paths */
 router.get('/dashboard', passportConfig.isAuthenticated, lessonController.dashboard);
 router.get('/explanation', passportConfig.isAuthenticated, lessonController.explanation);
-router.get('/demo/dashboard', passportConfig.isAuthenticated, lessonController.tutorial);
-router.get('/demo/story', passportConfig.isAuthenticated, lessonController.tutorial);
-router.get('/demo/practice', passportConfig.isAuthenticated, lessonController.tutorial);
+router.get('/dashboard/tutorial', passportConfig.isAuthenticated, lessonController.tutorial);
+router.get('/story/tutorial', passportConfig.isAuthenticated, lessonController.tutorial);
+router.get('/practice/tutorial', passportConfig.isAuthenticated, lessonController.tutorial);
 
 /* Get user`s lesson */
 router.get('/:story/:subj', passportConfig.isAuthenticated, lessonController.lesson);
